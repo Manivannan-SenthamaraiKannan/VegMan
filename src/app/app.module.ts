dcb8fa84d1c98b06005affbd6c9a030e75d29665
Manivannan-SenthamaraiKannan <sharvamanivannan@gmail.com>
2022-09-04 21:22:50 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/assets/images.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ImagesDirective } from './images.directive';
+
+describe('Directive: Images', () => {
+  it('should create an instance', () => {
+    const directive = new ImagesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
